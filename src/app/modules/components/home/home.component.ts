589c17099aa1ec946b8f9102e691194c25132a1a
Anele Mhlanga <anelenmhlanga@gmail.com>
2022-12-28 01:00:13 +0200

    consume getMovies method injected from MoviesService

--- src/app/modules/components/home/home.component.ts
+++ src/app/modules/components/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs';
+import { MoviesService } from 'src/app/core/services/movies.service';
+import { Movies } from '../../../core/types/movies.interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movies: Movies[];
 
+  constructor(private moviesService: MoviesService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.moviesService.getMovies().subscribe({
+      next: (response: any) => {
+        this.movies = response;
+        console.log('movies', this.movies);
+      },
+      error: (error) => {
+        console.error('error', error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
 }
