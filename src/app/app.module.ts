328d9e4e2b6d80c8f0bf08aa35aa281c67bd7839
Anele Mhlanga <anelenmhlanga@gmail.com>
2022-12-28 00:59:22 +0200

    add core module to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,18 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ModulesModule } from './modules/modules.module';
 import { CoreModule } from './core/core.module';
+import { MoviesService } from './core/services/movies.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     ModulesModule,
-    CoreModule
+    CoreModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MoviesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
