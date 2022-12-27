620978e8f7aa762f1f3fe3a8e39c6ef12fd5b1af
Anele Mhlanga <anelenmhlanga@gmail.com>
2022-12-27 23:14:24 +0200

    add shared module with footer/header components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-
-
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FooterComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [FooterComponent, HeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
