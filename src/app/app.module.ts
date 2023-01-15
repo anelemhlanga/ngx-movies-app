910c453c0392209a5bb4eedcab7a791d4f90c014
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-15 21:03:57 +0200

    import BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { SharedModule } from './shared/shared.module';
 import { ModulesModule } from './modules/modules.module';
 import { CoreModule } from './core/core.module';
 import { MoviesService } from './core/services/movies.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
     ModulesModule,
