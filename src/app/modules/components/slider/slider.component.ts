0e6931c53a68b169a57fe270008ff58da23db68f
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-15 20:24:21 +0200

    add Input Decorator to bind movies from HomeC

--- src/app/modules/components/slider/slider.component.ts
+++ src/app/modules/components/slider/slider.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movies } from '../../../core/types/movies.interfaces';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent {
-
+  @Input() movies: Movies[] = [];
 }
