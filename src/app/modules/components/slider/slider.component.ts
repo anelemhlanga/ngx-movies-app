9b3536170a4ab945e64c79bc4a33b7dd0cd7bc94
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-15 22:11:33 +0200

    add animations

--- src/app/modules/components/slider/slider.component.ts
+++ src/app/modules/components/slider/slider.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { Movies } from '../../../core/types/movies.interfaces';
 
@@ -5,6 +12,12 @@ import { Movies } from '../../../core/types/movies.interfaces';
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent {
   @Input() movies: Movies[] = [];
